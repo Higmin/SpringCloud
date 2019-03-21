ad22f5e1cbd4fecc404db1c05a8c11cb384e0294
Higmin <guojianmin0702@gmail.com>
2019-03-21 08:14:36 +0800

    springcloud 整合mybatis和mysql

--- consumer/src/main/java/com/zdhs/oms/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/zdhs/oms/consumer/ConsumerApplication.java
@@ -1,10 +1,7 @@
 package com.zdhs.oms.consumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
